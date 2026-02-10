879de307dbf632635a08fffa1d1139570163534b
MohdHudhaifa03 <mohamedhudhaifazubair@gmail.com>
2026-02-10 09:50:55 +0530

    notifications added

---
+++ src/app/admin/notifications/page.tsx
@@ -0,0 +1,15 @@
+import Layout from "@/components/layout/Layout";
+import NotificationsView from "@/components/notifications/NotificationsView";
+
+export default function AdminNotificationsPage() {
+
+    return (
+        <Layout>
+            <NotificationsView />
+        </Layout>
+    )
+    
+   
+    
+    }
+
